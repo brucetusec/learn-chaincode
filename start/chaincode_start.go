97026abf3dfe8800ece65d2804cda6475fd49299
Bruce Tu(凃仲蔚) <brucetu@gamania.com>
2017-01-12 00:34:34 +0800

    myGo

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -19,6 +19,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strconv"
+	
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -43,6 +45,18 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
+    err := stub.PutState("hello_world", []byte(args[0]))    
+    if err != nil {
+        return nil, err
+    }
+
+    var i uint64 = 0
+    s := strconv.FormatUint(i, 16)    
+    err = stub.PutState("worldCount",[]byte(s) )
+    if err != nil {
+        return nil, err
+    }
+
 	return nil, nil
 }
 
@@ -53,22 +67,71 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
-	}
+	}else if function == "addLog" {
+        return t.addLog(stub, args)
+    }
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+func (t *SimpleChaincode) addLog(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    
+    var err error
+    fmt.Println("running write()")
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 1. LogContent")
+    }
+    countValBytes, err := stub.GetState("worldCount")
+
+    u, err := strconv.ParseUint(string(countValBytes), 10, 64)
+    if err != nil {
+
+        var jsonResp string = "{\"Error\":\"Failed to get state for worldCount\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    var myIndex uint64 = u + 1
+    myIndexS:=strconv.FormatUint(myIndex, 16)
+	fmt.Println("myIndex is " + myIndexS)
+	
+    logContent := args[0]                            
+    err = stub.PutState(myIndexS, []byte(logContent))  //write the variable into the chaincode state
+    if err != nil {
+        return nil, err
+    }
+    return nil, nil
 }
 
 // Query is our entry point for queries
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
-	// Handle different functions
-	if function == "dummy_query" {											//read a variable
-		fmt.Println("hi there " + function)						//error
-		return nil, nil;
-	}
-	fmt.Println("query did not find func: " + function)						//error
+    // Handle different functions
+    if function == "read" {                            //read a variable
+        return t.read(stub, args)
+    }
+    fmt.Println("query did not find func: " + function)
 
-	return nil, errors.New("Received unknown function query: " + function)
+    return nil, errors.New("Received unknown function query: " + function)
 }
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
+
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
+}
+
